d1c487c10fc925c41b4684e50016cf8da53f561d
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2018-08-15 04:18:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,21 +12,6 @@ class App extends Component {
         </header>
         <h1 className="App-title">Yrwynmyr</h1>
         <p className="App-intro">.
-        </p>
-        <div className="top">
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/ZI54ajzIBvI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-		</div>
-		
-        <div className="left-column">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-hasx9Jx_6E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-         </div>
-         <div className="right-column">
-         <iframe width="560" height="315" src="https://www.youtube.com/embed/-FO4N5vbouE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        </div>
-		
-   
-   
-      </div>
     );
   }
 }
