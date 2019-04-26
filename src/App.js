e14cfb7552095303de73af6b5c5e75e9351df6f9
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2019-04-26 08:30:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-import sun from './sun.gif';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={sun} className="App-logo" alt="sun" />
-          
-        </header>
-        <h1 className="App-title">Yrwynmyr</h1>
-        <p className="App-intro">.</p>
-    );
-  }
+class App extends React Component{
+render(){
+return(      <div className="App">);
 }
+}
+
 
 export default App;
