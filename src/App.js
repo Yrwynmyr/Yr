402d9f92775b9cb7cab391344b24d81b3661c1c0
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2019-04-26 09:32:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-
-import '/App.css';
-
-
-Class App extends ReactComponent{
-render(){
-Return(<div className={App}
-<div><h1>Hello World</h1>
-</div>
-</div>);
+import React, { Component } from 'react';
+import Sun from './sun.gif';
+import TheHeights from './TheHeights.mp3';
+import Delusion from './Delusion.mp3';
+import IceBurgs from './IceBurgs.mp3';
+import './App.css';
 
 
 
+ 
+class App extends Component {
+  render() {
+    return (
+  <div className="App">
+    
+      <div className="Header">
+      <img className="Logo" src={Sun} alt="Logo"/>
+      <h1>Garrett Johnson</h1>
+      <h2>Profolio</h2>
+     </div><div>
 
+   </div>
+</div>  
+  
+    );
+  }
 }
 
+export default App;
 
-}
-
-export App;
