5788940455935a8a97d3130c905f1252660b4909
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2019-04-26 09:14:19 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import '/App.css';
+
+
+Class App extends ReactComponent{
+render(){
+Return(<div className={App}
+<div><h1>Hello World</h1>
+</div>
+</div>);
+
+
+
+
+}
+
+
+}
+
+export App;
